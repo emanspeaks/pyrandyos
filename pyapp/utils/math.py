7852e43723aeb47d5293ceed526068325466cf9e
Randy Eckman <emanspeaks@gmail.com>
2025-08-16 23:28:11 -0500

    add support for pandas series in round_half_away

--- pyapp/utils/math.py
+++ pyapp/utils/math.py
@@ -1,12 +1,18 @@
-def round_half_away(value: float | int | str | None = None,
+from pandas import Series
+from numpy import divmod as npdivmod
+
+
+def round_half_away(value: float | int | str | Series | None = None,
                     ndigits: int = 0):
-    value = float(value)
+    if not isinstance(value, Series):
+        value = float(value)
+
     sgn = 1 - 2*(value < 0)
     absval = abs(value)
     expo = -ndigits or 0
     scalar = 10**expo
-    quo, rem = divmod(absval, scalar)
+    quo, rem = npdivmod(absval, scalar)
     out = sgn*scalar*(quo + (rem*2 >= scalar))
     if ndigits <= 0:
-        return int(out)
+        return out.astype(int) if isinstance(out, Series) else int(out)
     return out
