cab1bc85c15348b2bba9adcb8486b47d7b58a0d4
Randy Eckman <emanspeaks@gmail.com>
2025-09-11 13:24:31 -0500

    renaming to pyrandyos

--- pyapp/__init__.py
+++
@@ -1,2 +0,0 @@
-from .version import __version__  # noqa: F401
-from .app import PyApp  # noqa: F401
--- pyapp/config/__init__.py
+++
@@ -1,2 +0,0 @@
-from .app import AppConfig  # noqa: F401
-from .local import get_local_config, save_local_config  # noqa: F401
